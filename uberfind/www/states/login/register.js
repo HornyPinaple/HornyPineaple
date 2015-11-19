48bc2bcd4f405c448ecd401239b32ef4d3aa6bf3
Dima Dodzin <d.dodzin@gmail.com>
2015-11-19 20:55:19 +0200

    Changed login page to only register

---
+++ uberfind/www/states/login/register.js
@@ -0,0 +1,26 @@
+(function(){	
+
+	function RegisterController($scope, $state, ubServerProxy){
+		if('userId' in localStorage){
+			$state.go('search');
+		}
+
+		$scope.lo = {};
+
+		$scope.register = function(){
+			ubServerProxy.registerUser($scope.lo.user, $scope.lo.phone)
+				.then(function(request){
+					localStorage.setItem('userId', request.data);
+					localStorage.setItem('name', $scope.lo.user);
+					localStorage.setItem('phone', $scope.lo.phone);
+					$state.go('search');
+				}, function(err){
+					$scope.err = err;
+				})
+		}
+	}
+
+	RegisterController.$inject = ['$scope', '$state', 'ubServerProxy'];
+
+	angular.module('uberfind').controller('RegisterController', RegisterController);
+})();
