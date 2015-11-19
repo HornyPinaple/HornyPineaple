af6d0f2f3030b899d447e99547bca34122ae39db
yuval-bichler <yuvalbic@gmail.com>
2015-11-19 20:48:34 +0200

    Implemented dbAccess module.

--- server/lib/dbAccess/index.js
+++ server/lib/dbAccess/index.js
@@ -1,23 +1,77 @@
 /**
  * Created by yuval_000 on 11/19/2015.
  */
-function DbAccess(){
+var q = require('q');
 
+function DbAccess(){
+    this.mongoClient = require('mongodb').MongoClient
+    this.db = null;
+    this.usersCollection = null;
 }
 
 
-DbAccess.prototype.connect = function (){
-
+DbAccess.prototype.connect = function (serverUrl,dbName){
+    var deferred = q.defer();
+    var self = this;
+    var url = 'mongodb://'+serverUrl+'/'+ dbName;
+    self.mongoClient.connect(url, function(err, db) {
+        if(err){
+            deferred.reject(err);
+        }else{
+            console.log("Connected correctly to server");
+            self.db = db;
+            self.usersCollection = db.collection('users');
+            deferred.resolve();
+        }
+    });
+    return deferred.promise;
 };
+
+
+DbAccess.prototype.disconnect = function(){
+    if(this.db){
+        this.db.close();
+    }
+}
+
 DbAccess.prototype.createUser = function(name,phoneNumber){
+    var deferred = q.defer();
 
+    var userJson = {
+        name:name,
+        phoneNumber:phoneNumber
+    };
+
+    this.usersCollection.insertOne(userJson,function(err,result){
+        if(err){
+            deferred.reject(err);
+        }else {
+            var insertedUserId = result["ops"][0]["_id"]
+            deferred.resolve(insertedUserId);
+        }
+    });
+    return deferred.promise;
 };
 
-DbAccess.prototype.upsertLocations = function(userGuid , currentLocation,destinationLocation){
+DbAccess.prototype.upsertLocations = function(userId , currentLocation,destinationLocation){
+    var deferred = q.defer();
 
+    var updateJson = {
+        currentLocation:currentLocation,
+        destinationLocation:destinationLocation
+    }
+
+    this.usersCollection.update({"_id":userId },updateJson,{"upsert":true},function(err,result){
+        if(err){
+            deferred.reject(err);
+        }else{
+            deferred.resolve();
+        }
+    });
+    return deferred.promise;
 };
 
-DbAccess.prototype.getCompaninons = function(userGuid){
+DbAccess.prototype.getCompaninons = function(userId){
 
 };
 
---
+++ server/lib/dbAccess/test.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yuval_000 on 11/19/2015.
+ */
+var config = {
+    mongodbServerUrl: 'horny-pineapple.cloudapp.net:27017',
+    dbName:'uberfind'
+};
+
+var DbAccess = require('../dbAccess');
+
+var dbAccess = new DbAccess();
+
+dbAccess.connect(config.mongodbServerUrl,config.dbName).then(function(){
+    dbAccess.createUser('yuval',0525392753).then(function(userId){
+        console.log(userId);
+        dbAccess.upsertLocations(userId,'loc1','loc2').then(function(){
+           console.log('Success.');
+        });
+    });
+})
